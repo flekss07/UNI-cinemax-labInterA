eb7fdaf9474326c67e0f007657fa6af84b784c77
loren <lorenzo.scalone@gmail.com>
2026-05-06 13:36:11 +0200

    Generazione classe film

--- cinemax/src/User.java
+++ cinemax/src/User.java
@@ -1,10 +1,35 @@
 import java.util.Date;
 
 public class User {
-    String Nome;
-    String Cognome;
-    String Password;
-    String Username;
-    Date DataDiNascita;
-    String Indirizzo;
+    private String Nome;
+    private String Cognome;
+    private String Password;
+    private String Username;
+    private Date DataDiNascita;
+    private String Indirizzo;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public String getCognome() {
+        return Cognome;
+    }
+    /* Da implementare la funzione di encryption */
+    public String getPassword(){
+        return Password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public Date getDataDiNascita() {
+        return DataDiNascita;
+    }
+
+    public String getIndirizzo() {
+        return Indirizzo;
+    }
 }
+
