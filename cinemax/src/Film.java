c46d12798b259e8bd37656341da1bc14945d44c6
loren <lorenzo.scalone@gmail.com>
2026-05-06 12:44:10 +0200

    Generazione classe film

--- cinemax/src/Film.java
+++ cinemax/src/Film.java
@@ -1,3 +1,34 @@
-public class Film {
+import java.util.Date;
 
+public class Film {
+    private Tag[] Generi;
+    private String Titolo;
+    private String Regista;
+    private Date Data;
+    private int durata;
+    private int etaMin;
+
+    public Tag[] getGeneri() {
+        return Generi;
+    }
+
+    public String getTitolo() {
+        return Titolo;
+    }
+
+    public String getRegista() {
+        return Regista;
+    }
+
+    public Date getData() {
+        return Data;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+    public int getEtàMin() {
+        return etàMin;
+    }
 }
+
