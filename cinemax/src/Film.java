e864c6146b6c1fcb7f6a083957854083d85b844d
udjj <elia.ricciolo@gmail.com>
2026-05-06 13:34:43 +0200

    Added Film Tags

--- cinemax/src/Film.java
+++ cinemax/src/Film.java
@@ -1,5 +1,5 @@
 import java.util.Date;
-
+//placeholder data names
 public class Film {
     private Genres[] Generi;
     private String Titolo;
@@ -7,7 +7,15 @@ public class Film {
     private Date Data;
     private int durata;
     private int etaMin;
-
+    //placeholder data names
+    public Film(String Titolo,Genres[] Genres, String Regista, Date Data, int durata, int etaMin){
+        this.Titolo = Titolo;
+        this.Generi = Genres;
+        this.Regista = Regista;
+        this.Data = Data;
+        this.durata = durata;
+        this.etaMin = etaMin;
+    }
     public Genres[] getGeneri() {
         return Generi;
     }
--- cinemax/src/User.java
+++ cinemax/src/User.java
@@ -7,4 +7,13 @@ public class User {
     String Username;
     Date DataDiNascita;
     String Indirizzo;
+    public User(String Nome, String Cognome, String Password, String Username, Date DataDiNascita) {
+        this.Nome = Nome;
+        this.Cognome = Cognome;
+        this.Password = Password;
+        this.Username = Username;
+        this.DataDiNascita = DataDiNascita;
+        this.Indirizzo = "";
+    }
+
 }
