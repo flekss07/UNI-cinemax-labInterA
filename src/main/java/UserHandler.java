396bb5cfe6af633ef4d2370871ab5b9aec924cc2
eliaccio <elia.ricciolo@gmail.com>
2026-05-09 18:28:23 +0200

    Aggiunto check di username già esistenti

--- src/main/java/UserHandler.java
+++ src/main/java/UserHandler.java
@@ -8,12 +8,19 @@ public class UserHandler {
     }
 
     /*Metodo x registrare l'utente*/
-    public void addUser(){
-
+    public void addUser(User newUser){
+        logUser(newUser);
+               
     }
+    
     /* Metodo per controllare se l'utente esiste già*/
-    public void logUser(){
-
+    public void logUser(User newUser){
+        for (int i = 0; i < userList.size(); i++) {
+            User newUser2=userList.get(i);
+            if(newUser2.getUsername()==newUser.getUsername()){
+                System.err.println("Utente già esistente");
+            }
+        }
     }
 
 
