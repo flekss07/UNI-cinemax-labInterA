f565f11d152f644e61e8e7d167502c592d4c3ad9
eliaccio <elia.ricciolo@gmail.com>
2026-05-10 10:53:17 +0200

    sistemazione e correzione funzione checkUser

--- src/main/java/UserHandler.java
+++ src/main/java/UserHandler.java
@@ -4,22 +4,21 @@ public class UserHandler {
     private LinkedList<User> userList = new LinkedList<>();
     User newUser;
     /*Funzione x registrare l'utente*/
-    public boolean addUser(User user) {
-        // Controlliamo se l'username è già preso
-        for (User u : userList) {
+    public void addUser(User user) {
+        
+        userList.add(user);
+    }
+    /* funzione per controllare se l'utente esiste già*/
+    public void checkUser(User user){
+    for (User u : userList) {
             if (u.getUsername().equals(user.getUsername())) {
-                System.out.println("Errore: Username già esistente.");
-                return false;
+                // to change into throw exception (non bloccante)
+                System.err.println("Errore: Username già esistente.");
             }
         }
-        userList.add(user);
-        return true;
     }
 }
-/* funzione per controllare se l'utente esiste già*/
-public void logUser(){
- String testSync;
-}
+
 
 
 
