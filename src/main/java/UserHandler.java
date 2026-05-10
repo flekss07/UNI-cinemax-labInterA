1479e92fd3674769b275695f030e032822edff38
loren <lorenzo.scalone@gmail.com>
2026-05-10 10:47:21 +0200

    User Handler inizio

--- src/main/java/UserHandler.java
+++ src/main/java/UserHandler.java
@@ -1,29 +1,25 @@
 import java.util.LinkedList;
 
 public class UserHandler {
-    private LinkedList<User> userList;
-
-    public UserHandler(LinkedList<User> userList){
-        this.userList=userList;
-    }
-
-    /*Metodo x registrare l'utente*/
-    public void addUser(User newUser){
-        logUser(newUser);
-               
-    }
-    
-    /* Metodo per controllare se l'utente esiste già*/
-    public void logUser(User newUser){
-        for (int i = 0; i < userList.size(); i++) {
-            User newUser2=userList.get(i);
-            if(newUser2.getUsername()==newUser.getUsername()){
-                //to update into trow UserAlreadyExistingException
-                System.err.println("Utente già esistente");
+    private LinkedList<User> userList = new LinkedList<>();
+    User newUser;
+    /*Funzione x registrare l'utente*/
+    public boolean addUser(User user) {
+        // Controlliamo se l'username è già preso
+        for (User u : userList) {
+            if (u.getUsername().equals(user.getUsername())) {
+                System.out.println("Errore: Username già esistente.");
+                return false;
             }
         }
+        userList.add(user);
+        return true;
     }
-
-
-
 }
+/* funzione per controllare se l'utente esiste già*/
+public void logUser(){
+ String testSync;
+}
+
+
+
