54a87fa8c909dbd916dc5a90b1949e2994ae8634
loren <lorenzo.scalone@gmail.com>
2026-05-10 11:36:03 +0200

    Riscrittura classe AESencrypt

--- src/main/java/AESencrypt.java
+++ src/main/java/AESencrypt.java
@@ -1,30 +1,28 @@
-import javax.crypto.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
-public  class AESencrypt {
-    private String Password; //getPassword();
+public class AESencrypt {
+    private static final String FIXED_KEY = "cinemax2026key!"; // 16 caratteri per AES-128
 
-    public AESencrypt(String Password) {
-        this.Password = Password;
-    }
-
-    public String encryptPassword() throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-        KeyGenerator Chiave = KeyGenerator.getInstance("AES");
-        Chiave.init(256);
-        //Chiave segreta
-        SecretKey chiaveSegreta = Chiave.generateKey();
-        //AES Cipher instance
+    // Prende in input una password e restituisce la password codificata
+    public static String encrypt(String password) throws Exception {
+        SecretKeySpec chiaveSegreta = new SecretKeySpec(
+                FIXED_KEY.getBytes(), 0, 16, "AES");
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, chiaveSegreta);
-        //Inizio encryption
-        byte[] encryptedpass = cipher.doFinal(Password.getBytes());
-        //encode in base64
-        String passb64 = Base64.getEncoder().encodeToString(encryptedpass);
-        return passb64;
+        byte[] encryptedpass = cipher.doFinal(password.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedpass);
     }
 
+    // Prende in input una password codificata e restituisce la password originale
+    public static String decrypt(String passwordCodificata) throws Exception {
+        SecretKeySpec chiaveSegreta = new SecretKeySpec(
+                FIXED_KEY.getBytes(), 0, 16, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, chiaveSegreta);
+        byte[] decodedPassword = Base64.getDecoder().decode(passwordCodificata);
+        byte[] decryptedPassword = cipher.doFinal(decodedPassword);
+        return new String(decryptedPassword);
+    }
 }
-
-
