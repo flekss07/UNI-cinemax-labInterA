9085368a9437d8b3d0e9f9ac951876efbbb4204d
loren <lorenzo.scalone@gmail.com>
2026-05-10 12:13:48 +0200

    Gestione errori linea vuota intera classe userhandler

--- src/main/java/UserHandler.java
+++ src/main/java/UserHandler.java
@@ -2,31 +2,59 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class UserHandler {
-    private LinkedList<User> userList = new LinkedList<>();
+    private final LinkedList<User> userList = new LinkedList<>();
 
     /*Funzione x registrare l'utente*/
     public void addUser() throws Exception{ 
         Scanner s= new Scanner(System.in);
         User newUser = new User();
-        boolean x=true;
+        boolean x=true; //variabile di controllo sui cicli while
 
         //da inserire gestione errore nel caso si eviti di inserire il campo
-        System.out.println("Inserire Nome"); 
-        String nome = s.nextLine();
-        newUser.setNome(nome);
+        while(x) {
+            System.out.println("Inserire Nome");
+            String nome = s.nextLine();
+            if(!nome.isEmpty() &&!nome.trim().isEmpty()) {
+            System.out.println("Si prega di inserire un nome valido");
+            } else{
+                newUser.setNome(nome);
+                x= false;
+            }
+        }
+        x= true;
+        while(x) {
+            System.out.println("Inserire Cognome");
+            String cognome = s.nextLine();
+            if(!cognome.isEmpty() &&!cognome.trim().isEmpty()) {
+                System.out.println("Si prega di inserire un cognome valido");
+            } else{
+                newUser.setCognome(cognome);
+                x= false;
+            }
+        }
+        x = true;
+        while(x) {
+            System.out.println("Inserire Username");
+            String username = s.nextLine();
+            if(!username.isEmpty() &&!username.trim().isEmpty()) {
+                System.out.println("Si prega di inserire un username valido");
+            } else{
+            newUser.setUsername(username);
+            x= false;
+            }
+        }
+        x = true;
+        while(x) {
+            System.out.println("Inserire indirizzo di residenza");
+            String indirizzo = s.nextLine();
+            if(!indirizzo.isEmpty() &&!indirizzo.trim().isEmpty()) {
+                System.out.println("Si prega di inserire un indirizzo valido");
+            }else{
+                    newUser.setIndirizzo(indirizzo);
+            x= false;}
 
-        System.out.println("Inserire Cognome"); 
-        String cognome = s.nextLine();
-        newUser.setCognome(cognome);
 
-        System.out.println("Inserire Username");
-        String username=s.nextLine();
-        newUser.setUsername(username);
-
-        System.out.println("Inserire indirizzo di residenza");
-        String indirizzo=s.nextLine();
-        newUser.setIndirizzo(indirizzo);
-
+        }
         //da inserire gestione della data
         System.out.println("inserire la data con formato gioni/mesi/anni");
 
@@ -35,19 +63,20 @@ public class UserHandler {
 
         
         //inserimento della password 
-        while (x=true) {
+        while (x) {
+            System.out.println("inserire una password");
+            String password = s.nextLine();
 
-        // da inserire un secondo inserimento della password per evitare errori di digitazione
-        System.out.println("inserire una password");
-        String password = s.nextLine();
-
-        System.out.println("inserire nuovamente la password");
-        String passcmp = s.nextLine();
-
-        if(password==passcmp){}
+            System.out.println("inserire nuovamente la password");
+            String passcmp = s.nextLine();
+        if (password.isEmpty() || password.trim().isEmpty()) {
+            System.out.println("Si prega di inserire una password valida");
+        } else if (!password.equals(passcmp)) {
+            System.out.println("Le password non corrispondono, riprova.");
+        } else if(password.equals(passcmp)){
             //encrypting della password
             AESencrypt crypted = new AESencrypt();
-            newUser.setPassword(crypted.encrypt(password));
+            newUser.setPassword(AESencrypt.encrypt(password));
 
             x=false;
         }
