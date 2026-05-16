d72695c74a78dbe1797d5729db7590137f5cec76
flekss07 <edopia007@gmail.com>
2026-05-16 11:24:11 +0200

    aggiunto metodo per cancellare una prenotazione in base a id e data

--- src/main/java/PrenotazioniHandler.java
+++ src/main/java/PrenotazioniHandler.java
@@ -1,5 +1,6 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.UUID;
 
@@ -24,5 +25,19 @@ public class PrenotazioniHandler {
         for(Prenotazione p: this.prenList)
             if(p.getUsername().equals(username)) foundList.add(p);
         return foundList; // ritorna una lista vuota se non trova prenotazioni associate
+    }
+
+    //metodo di cancellamento di una prenotazione
+    public boolean eliminaPrenotazione(String id){
+        Iterator<Prenotazione> prenIt = this.prenList.iterator();// crea un iteratore della lista per poterla modificare mentre viene iterata (non si puo fare con foreach)
+        while (prenIt.hasNext()){// continua a iterare la lista fino a che non arriva alla fine o non viene trovato un oggetto prenotazione
+            Prenotazione p = prenIt.next(); // prende oggetto successivo per check
+            if(p.getId().equals(id) && p.getDate().isBefore(LocalDateTime.now())) {
+                prenIt.remove();
+                this.fh.savePrenList(this.prenList); // salva cambiamento
+                return true; // conferma rimozione
+            }
+        }
+        return false; // conferma di non aver trovato la prenotazione da rimuovere
     }
 }
